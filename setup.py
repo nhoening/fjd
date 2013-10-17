1aaa83df7c84138c1c1dd2c74201c4742662ca83
nhoening <nhoening@gmail.com>
2013-10-17 20:25:43 +0200

    include TODO in longdescription

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README.md', 'CHANGES.txt')
+long_description = read('README.md', 'CHANGES.txt', 'TODO')
 
 class PyTest(TestCommand):
     def finalize_options(self):
