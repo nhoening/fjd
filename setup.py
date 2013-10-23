01f3a72680b1e16aeb4421471fa8827592a80478
nhoening <nhoening@gmail.com>
2013-10-23 12:24:35 +0200

    closes #6 last step needed to get installation working (first was avoiding recursive dependency  nf fjd)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-long_description = read('README.md') + read('CHANGES.txt') + read('TODO')
+long_description = read('README.md') + read('CHANGES.md')
 
 class PyTest(TestCommand):
     def finalize_options(self):
